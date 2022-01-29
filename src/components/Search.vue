7ad6b1c9353de3aa388386fff983edab66b53f7b
callum buttery <callumbuttery@icloud.com>
2022-01-29 22:05:22 +0000

    add search button to modal

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -3,21 +3,38 @@
     <v-row class="justify-center" no-gutters>
       <v-card>
         <v-text-field
-          :rules="rules"
           hide-details="auto"
           class="centered-input"
           outline
           single-line
           placeholder="Search Location"
+          v-model="input"
         ></v-text-field
       ></v-card>
     </v-row>
+    <v-btn class="justify-center" color="secondary" elevation="4" v-on:click="handleInput">Search</v-btn>
   </v-container>
 </template>
 
 <script>
 export default {
   name: "Search",
+
+  data() {
+    return {
+      input: "",
+    }
+  },
+  methods: {
+    handleInput: function() {
+      if (!this.input) {
+        alert("Please enter a valid location");
+        return;
+      }
+
+      
+    }
+  }
 };
 </script>
 
